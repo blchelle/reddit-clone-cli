cbf0100293f35dd9d68e6fc6bc0863122c2ebece
blchelle <brocklchelle@gmail.com>
2020-11-24 16:40:35 -0700

    Fixed the anon-user post field bug

--- phase2/models/MainModel.py
+++ phase2/models/MainModel.py
@@ -33,39 +33,49 @@ class MainModel(model.Model):
 		latestIDs = posts.find().sort([("$natural",-1)]).limit(1)
 		for doc in latestIDs:
 			latestID = doc["Id"]
-		
+
 		newID = int(latestID)+1
 
 		tags =""
-		
+
 		for tag in tagsList:
 
-			tag_exist = tags_collection.find({"TagName":tag})
+			tag_exist = tags_collection.find( { "TagName": tag } )
 			if(tag_exist.count() != 0):
 				for doc in tag_exist:
-					tags_collection.update_one({"_id":doc["_id"]},{"$set":{"Count":doc["Count"]+1}})
+					tags_collection.update_one(
+						{
+							"_id": doc["_id"]
+						},
+						{
+							"$set": { "Count": doc["Count"] + 1 }
+						}
+					)
 			else:
 				latestTagID=0
-				latestTagIDs = tags_collection.find().sort([("$natural",-1)]).limit(1)
+				latestTagIDs = tags_collection.find().sort( [ ( "$natural", -1) ] ).limit(1)
+
 				for doc in latestTagIDs:
 					latestTagID = doc["Id"]
 				newTagID = int(latestTagID)+1
-				tags_collection.insert({
-					"Id": str(newTagID),
-					"TagName": tag,
-					"Count": 1
-					})
 
-			
+				tags_collection.insert(
+					{
+						"Id": str(newTagID),
+						"TagName": tag,
+						"Count": 1
+					}
+				)
+
+
 			tags+="<"+tag+">"
 
-		posts.insert({
+		documentFields = {
 			"Id":str(newID),
 			"PostTypeId": "1",
 			"CreationDate":str(datetime.now().isoformat()),
 			"Title": title,
 			"Body":body,
-			"OwnerUserId": poster,
 			"Tags": tags,
 			"Score": 0,
 			"ViewCount": 0,
@@ -73,9 +83,14 @@ class MainModel(model.Model):
 			"CommentCount": 0,
 			"FavoriteCount": 0,
 			"ContentLicense": "CC BY-SA 2.5"
-			})
-		
-		
+		}
+
+		if poster != -1:
+			documentFields.update( { 'OwnerUserId': poster } )
+
+		posts.insert(documentFields)
+
+
 	test="test"
 	dbname = "291db"
 	def findQuestions(self, searchString):
@@ -95,9 +110,17 @@ class MainModel(model.Model):
 			patternList.append(pattern)
 		db = self.client[self.dbname]
 		posts = db["Posts"]
-		buffer = posts.find({"PostTypeId":"1", '$or':[
-		{"Title":{'$in': patternList}},
-		{"Body":{'$in': patternList}},
-		{"Tags":{'$in': patternList}}]})
+		buffer = posts.find(
+			{
+				"PostTypeId":"1",
+				'$or':
+				[
+					{ "Title": { '$in': patternList } },
+					{ "Body":  { '$in': patternList } },
+					{ "Tags":  { '$in': patternList } }
+				]
+			}
+		)
+
 		results.extend(buffer)
 		return results
