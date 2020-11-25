1d8cc615f6df6be95cda7fa2316cd15fd9ac1d35
DJMaest <sazimi@ualberta.ca>
2020-11-24 18:22:54 -0700

    enhance search

--- phase2/models/MainModel.py
+++ phase2/models/MainModel.py
@@ -3,97 +3,119 @@ from datetime import datetime
 import uuid
 import sqlite3
 import re
+import json
 
 class MainModel(model.Model):
 
-	def postQuestion(self, title, body, tagsList, poster):
-		"""
-		inserts question posts into the database
+    def postQuestion(self, title, body, tagsList, poster):
+        """
+        inserts question posts into the database
 
 
-		Parameters
-		----------
-		title : str
-			title of question
-		body : str
-			body of question
-		poster: str
-			username of poster
+        Parameters
+        ----------
+        title : str
+            title of question
+        body : str
+            body of question
+        poster: str
+            username of poster
 
-		Return
-		----------
-		None or {}
+        Return
+        ----------
+        None or {}
 
-		"""
+        """
 
-		db = self.client["291db"]
-		posts = db["Posts"]
-		tags_collection = db["Tags"]
-		latestID=0
-		latestIDs = posts.find().sort([("$natural",-1)]).limit(1)
-		for doc in latestIDs:
-			latestID = doc["Id"]
-		
-		newID = int(latestID)+1
+        db = self.client["291db"]
+        posts = db["Posts"]
+        tags_collection = db["Tags"]
+        latestID=0
+        latestIDs = posts.find().sort([("$natural",-1)]).limit(1)
+        for doc in latestIDs:
+            latestID = doc["Id"]
 
-		tags =""
-		
-		for tag in tagsList:
+        newID = int(latestID)+1
 
-			tag_exist = tags_collection.find({"TagName":tag})
-			if(tag_exist.count() != 0):
-				for doc in tag_exist:
-					tags_collection.update_one({"_id":doc["_id"]},{"$set":{"Count":doc["Count"]+1}})
-			else:
-				latestTagID=0
-				latestTagIDs = tags_collection.find().sort([("$natural",-1)]).limit(1)
-				for doc in latestTagIDs:
-					latestTagID = doc["Id"]
-				newTagID = int(latestTagID)+1
-				tags_collection.insert({
-					"Id": str(newTagID),
-					"TagName": tag,
-					"Count": 1
-					})
+        tags =""
 
-			
-			tags+="<"+tag+">"
+        for tag in tagsList:
 
-		posts.insert({
-			"Id":str(newID),
-			"PostTypeId": "1",
-			"CreationDate":str(datetime.now().isoformat()),
-			"Title": title,
-			"Body":body,
-			"OwnerUserId": poster,
-			"Tags": tags,
-			"Score": 0,
-			"ViewCount": 0,
-			"AnswerCount": 0,
-			"CommentCount": 0,
-			"FavoriteCount": 0,
-			"ContentLicense": "CC BY-SA 2.5"
-			})
-		
-		
-	test="test"
-	dbname = "291db"
-	def findQuestions(self, searchString):
-		"""
-		find questions based on keywords
+            tag_exist = tags_collection.find({"TagName":tag})
+            if(tag_exist.count() != 0):
+                for doc in tag_exist:
+                    tags_collection.update_one({"_id":doc["_id"]},{"$set":{"Count":doc["Count"]+1}})
+            else:
+                latestTagID=0
+                latestTagIDs = tags_collection.find().sort([("$natural",-1)]).limit(1)
+                for doc in latestTagIDs:
+                    latestTagID = doc["Id"]
+                newTagID = int(latestTagID)+1
+                tags_collection.insert({
+                    "Id": str(newTagID),
+                    "TagName": tag,
+                    "Count": 1
+                    })
 
-		Returns
-		-------
-		list of matching questions
 
-<<<<<<< HEAD
+            tags+="<"+tag+">"
+
+        posts.insert({
+            "Id":str(newID),
+            "PostTypeId": "1",
+            "CreationDate":str(datetime.now().isoformat()),
+            "Title": title,
+            "Body":body,
+            "OwnerUserId": poster,
+            "Tags": tags,
+            "Score": 0,
+            "ViewCount": 0,
+            "AnswerCount": 0,
+            "CommentCount": 0,
+            "FavoriteCount": 0,
+            "ContentLicense": "CC BY-SA 2.5"
+            })
+
+
+    test="test"
+    dbname = "291db"
+    def findQuestions(self, searchString):
+        """
+        find questions based on keywords
+
+        Returns
+        -------
+        list of matching questions
+
         """
         searchExpr = searchString.split(" ")
         patternList=[]
         results = []
+
+        useIndexSearch = True
         for keyWord in searchExpr:
-            pattern = re.compile(".*" + keyWord + ".*", re.IGNORECASE)
-            patternList.append(pattern)
+            if len(keyWord)<3:
+                useIndexSearch = False
+        for keyWord in searchExpr:
+            if useIndexSearch:
+                pattern = (keyWord).lower()
+                db = self.client[self.dbname]
+                posts = db["Posts"]
+
+                buffer = posts.find({"PostTypeId":"1", "Terms":pattern})
+
+                for p in buffer:
+                    if p not in results:
+                        results.append(p)
+
+            else:
+                break
+
+
+        if useIndexSearch:
+
+            return (results)
+
         db = self.client[self.dbname]
         posts = db["Posts"]
         buffer = posts.find({"PostTypeId":"1", '$or':[
@@ -102,20 +124,3 @@ class MainModel(model.Model):
         {"Tags":{'$in': patternList}}]})
         results.extend(buffer)
         return results
-=======
-		"""
-		searchExpr = searchString.split(" ")
-		patternList=[]
-		results = []
-		for keyWord in searchExpr:
-			pattern = re.compile(".*" + keyWord + ".*", re.IGNORECASE)
-			patternList.append(pattern)
-		db = self.client[self.dbname]
-		posts = db["Posts"]
-		buffer = posts.find({"PostTypeId":"1", '$or':[
-		{"Title":{'$in': patternList}},
-		{"Body":{'$in': patternList}},
-		{"Tags":{'$in': patternList}}]})
-		results.extend(buffer)
-		return results
->>>>>>> 003cfde80eff6990307e0d8cdf306150d1a55f80
